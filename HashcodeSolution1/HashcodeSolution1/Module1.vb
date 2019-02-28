22aa862a8a53f83147e68db8910d15e1a050acab
ishaanzaveri <39041074+ishaanzaveri@users.noreply.github.com>
2019-03-01 00:38:35 +0530

    Update Module1.vb

--- HashcodeSolution1/HashcodeSolution1/Module1.vb
+++ HashcodeSolution1/HashcodeSolution1/Module1.vb
@@ -4,6 +4,40 @@
         'Zaveri In
         'Stark in
         'Rayhaan In
-    End Sub
 
+
+
+    End Sub
+    Sub Brain()
+        Dim num1, num2 As Integer
+        Dim maxTagsfiles As Integer = 0
+        Dim maxScoreGetter As Integer = 1
+        Dim maxScoreOptained As Integer
+        Dim currentScore As Integer
+        Dim counter As Integer = 0
+        For i = 0 To NumPhotos - 1
+            If ArrayIN(i, 1) > ArrayIN(maxTagsfiles, 1) Then
+                maxTagsfiles = i
+            End If
+        Next
+
+        num1 = maxTagsfiles
+        num2 = 0
+        Do
+
+            maxScoreOptained = Calc_Score(num1, num2)
+            For i = 2 To NumPhotos - 1
+                num2 = i
+                currentScore = Calc_Score(num1, num2)
+                If currentScore <> -1 Then
+                    If currentScore > maxScoreOptained Then
+                        maxScoreOptained = currentScore
+                        maxScoreGetter = i
+                        counter = counter + 1
+                    End If
+                End If
+            Next
+            num1 = maxScoreGetter
+        Loop Until counter = 0
+    End Sub
 End Module
