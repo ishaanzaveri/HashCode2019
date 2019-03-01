d56e272eb368ce1ece87115c191a0ea4604eff25
ishaanzaveri <39041074+ishaanzaveri@users.noreply.github.com>
2019-03-02 02:52:55 +0530

    Changes everywhere

--- HashcodeSolution1/HashcodeSolution1/Module1.vb
+++ HashcodeSolution1/HashcodeSolution1/Module1.vb
@@ -6,8 +6,8 @@ Module Module1
     Dim column As Integer
     Dim pic_info As String
     Dim slength As Integer
-    Dim ArrayIN(7999, 99) As String
-    Dim ArrayOUT(8000)
+    Dim ArrayIN(7999, 50) As String
+    Dim ArrayOUT(8000) As Integer
 
 
     Sub Main()
@@ -15,7 +15,8 @@ Module Module1
         'Stark in
         FileReading()
         Brain()
-
+        FileOutput()
+        Console.WriteLine("Done")
         Console.ReadLine()
     End Sub
 
@@ -23,58 +24,39 @@ Module Module1
 
         Dim FileLine As String = ""
         Dim fileReader As IO.StreamReader
-        Dim LastSpace As Integer = 2
-        Dim StringTaken As String = ""
+        Dim LastSpace As Integer = 0
+        Dim writetoarray As String = ""
         Dim Counterj As Integer = 0
-        Dim SpaceCounter As Integer = 2
-        Dim PhotoNumber As Integer = 0
-        Dim CurrentSpace As Integer = 0
-        Dim counter As Integer = 0
-        Dim column As Integer = 2
-        Dim WritetoArray As String = ""
-        Dim SpacePositions(500) As Integer
+        Dim CounterNums As Integer = 0
+        Dim RideNo As Integer = 0
 
         fileReader = New IO.StreamReader("b_lovely_landscapes.txt")
         fileReader.ReadLine()
 
-        'Do While fileReader.EndOfStream = False
-
-        'FileLine = fileReader.ReadLine()
-        '
-        'LastSpace = 0
-        'SpaceCounter = 0
-
-        'For i = 1 To Len(FileLine)
-        'If Mid(FileLine, i, 1) = " " Then
-
-        '   SpaceCounter = SpaceCounter + 1
-        '  SpacePositions(SpaceCounter) = i
-
-        'ed If
-
-        'Next
-
         For PhotoNumber = 0 To 7999
             ArrayIN(PhotoNumber, 0) = 0
         Next
-
-        For PhotoNumber = 0 To 7999
+        For counter = 0 To 7999
             FileLine = fileReader.ReadLine()
-            ArrayIN(PhotoNumber, 1) = Left(FileLine, 1)
-            LastSpace = 2
-            For counter = 3 To Len(FileLine)
-                If Mid(FileLine, counter, 1) = " " Then
-                    column = column + 1
-                    CurrentSpace = counter
-                    WritetoArray = Mid(FileLine, counter, (CurrentSpace - 1) - (LastSpace + 1))
-                    ArrayIN(PhotoNumber, column) = WritetoArray
-                    WritetoArray = ""
+            'Console.WriteLine(FileLine)
+            LastSpace = 0
+            CounterNums = 1
+            For i = 1 To Len(FileLine)
+                If Mid(FileLine, i, 1) = " " Then
+                    writetoarray = Mid(FileLine, LastSpace + 1, i - LastSpace - 1)
+                    LastSpace = i
+                    ArrayIN(Counterj, CounterNums) = writetoarray
+                    CounterNums = CounterNums + 1
+                End If
+                If i = Len(FileLine) Then
+                    writetoarray = Right(FileLine, i - LastSpace)
+                    ArrayIN(Counterj, CounterNums) = writetoarray
                 End If
             Next
-
-            column = 2
+            ArrayIN(Counterj, 6) = Counterj
+            Counterj = Counterj + 1
         Next
-        fileReader.Close()
+
     End Sub
     Sub Brain()
         Dim num1, num2 As Integer
@@ -90,17 +72,18 @@ Module Module1
                 maxTagsfiles = i
             End If
         Next
-
+        ArrayIN(maxTagsfiles, 0) = 1
         num1 = maxTagsfiles
         num2 = 0
         Do
+            counter = 0
             ArrayOUT(SlideCounter) = num1
             maxScoreOptained = Calculate_Score(num1, num2)
             For i = 2 To photo_num
                 If ArrayIN(i, 0) <> "1" Then
                     num2 = i
                     currentScore = Calculate_Score(num1, num2)
-                    If currentScore <> 0 Then
+                    If currentScore > 0 Then
                         If currentScore > maxScoreOptained Then
                             maxScoreOptained = currentScore
                             maxScoreGetter = i
@@ -109,7 +92,9 @@ Module Module1
                     End If
                 End If
             Next
+            SlideCounter = SlideCounter + 1
             num1 = maxScoreGetter
+            ArrayIN(maxScoreGetter, 0) = 1
             TotalSlides = TotalSlides + 1
         Loop Until counter = 0
         ArrayOUT(0) = TotalSlides
@@ -127,34 +112,23 @@ Module Module1
         Dim counter2 As Integer = 4
         Dim greatest As Integer = 0
         Dim min As Integer = 0
-        Dim found As Boolean = False
-        Do While ArrayIN(PhotoID_1, counter1) <> "" 'counter = m1 vice versa 
+        Dim found As String
+        Do While ArrayIN(PhotoID_1, m1) <> ""
             m1 = m1 + 1
-            counter1 = counter1 + 1
         Loop
-        Do While ArrayIN(PhotoID_2, counter2) <> "" ' counter = m2 vice versa 
+        Do While ArrayIN(PhotoID_2, m2) <> ""
             m2 = m2 + 1
-            counter2 = counter2 + 1
         Loop
-        For i = 1 To m1
-            For j = 1 To m2
-                If ArrayIN(PhotoID_1, i + 4) = ArrayIN(PhotoID_2, j + 4) Then
-                    found = True
+        For i = 0 To m1 - 1
+            For j = 0 To m2 - 1
+                If ArrayIN(PhotoID_1, i + 4) = ArrayIN(PhotoID_2, j + 4) And ArrayIN(PhotoID_1, i + 4) <> "" Then
+                    common_counter = common_counter + 1
+                    found = ArrayIN(PhotoID_1, i + 4)
                 End If
             Next
-            If found = True Then
-                common_counter = common_counter + 1 ' shouldnt this be inside 
-            Else
-                f_photo_tags = f_photo_tags + 1
-            End If
-            found = False
         Next
-        If m1 > m2 Then ' equal then m2 ?
-            greatest = m1
-        Else
-            greatest = m2
-        End If
-        s_photo_tags = greatest - (f_photo_tags + common_counter)
+        f_photo_tags = m1 - common_counter
+        s_photo_tags = m2 - common_counter
         If (common_counter < f_photo_tags) And (common_counter < s_photo_tags) Then
             min = common_counter
         ElseIf (f_photo_tags < common_counter) And (f_photo_tags < s_photo_tags) Then
@@ -165,6 +139,14 @@ Module Module1
         Calculate_Score = min
     End Function
     Sub FileOutput()
-
+        Dim filewriter As IO.StreamWriter
+        filewriter = New IO.StreamWriter("b_lovely_landscapes.out")
+        Dim counter As Integer = 0
+        filewriter.WriteLine(ArrayOUT(0))
+        For i = 1 To ArrayOUT(0)
+            filewriter.WriteLine(ArrayOUT(i) - 1)
+            ' counter = counter + 1
+        Next
+        filewriter.Close()
     End Sub
 End Module
